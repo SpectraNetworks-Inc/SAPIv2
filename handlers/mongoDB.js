0d631a32552ce1c82edf70467c406f9a123e7a85
tyree-z <61678444+tyree-z@users.noreply.github.com>
2021-02-02 18:51:01 -0800

    Mongo CRUD Operations
    Making my own functions to make things simpler i hope :+1:

--- handlers/mongoDB.js
+++ handlers/mongoDB.js
@@ -11,9 +11,53 @@ module.exports.io = (collection, document) => {
     try {
         client.connect();
         client.db('sapi').collection(collection).insertOne(document);
-        console.log(`Inserted ${document} into ${collection} in SAPI DB `);
+        console.log(`Inserted ${JSON.stringify(document)} into ${collection} in SAPI DB `);
     } 
     catch {
         console.log('Failed to Insert Data Into DB');
+    }
+};
+
+//Find All in collection and return MondoDB
+module.exports.fa = (collection) => {
+    try {
+        client.connect();
+        return client.db('sapi').collection(collection).find({});
+    } 
+    catch {
+        console.log('Failed to Find Data in Collection');
+    }
+};
+
+//Find One in collection and return first document
+module.exports.fo = (collection, query) => {
+    try {
+        client.connect();
+        return client.db('sapi').collection(collection).findOne(query);
+    } 
+    catch {
+        console.log('Failed to Find Data in Collection');
+    }
+};
+
+//Find One and update in collection
+module.exports.foau = (collection, queryFind, queryReplace) => {
+    try {
+        client.connect();
+        return client.db('sapi').collection(collection).findOneAndUpdate(queryFind, queryReplace);
+    } 
+    catch {
+        console.log('Failed to Find and update Data in Collection');
+    }
+};
+
+//Find One and delete in collection
+module.exports.foad = (collection, query) => {
+    try {
+        client.connect();
+        return client.db('sapi').collection(collection).findOneAndDelete(query);
+    } 
+    catch {
+        console.log('Failed to Find and update Data in Collection');
     }
 };
--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const DB = require('./handlers/mongoDB');
 
+
+DB.fo('test1',{Yeet:'Yeetus'}).then(function (a){
+    console.log(a);
+})
+
+const test = DB.fa('test1').toArray().then(function (results){
+    console.log(results);
+});
